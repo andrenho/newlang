c9573ed15b00a43ae2f9366fc4699e945079ae01
André Wagner <andre.nho@gmail.com>
2016-04-21 15:37:55 -0300

    .

--- tests/tests.cc
+++ tests/tests.cc
@@ -1,16 +1,8 @@
-#include <cstdlib>
 #include <iostream>
-#include <string>
 using namespace std;
 
 int main() 
 {
-    string* s = new string("abc");
-    cout << "TAP version 13" << endl;
-    cout << "1..3" << endl;
-    cout << "ok 1 Test #1" << endl;
-    cout << "ok 2 Test # Directive" << endl;
-    cout << "ok 3 Test #3" << endl;
 }
 
 // vim: ts=4:sw=4:sts=4:expandtab:foldmethod=marker:syntax=cpp
